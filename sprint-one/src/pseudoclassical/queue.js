d23034b91ed55c5e84de08b2297b345f7b1121ac
Hack Reactor Students <forrestbthomas@gmail.com>
2014-06-05 13:19:16 -0700

    passed pseudo-classical with lots of style

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.length = 0;
+  this.storage = {};
+};
+
+Queue.prototype.enqueue = function(value){
+  this.storage[this.length] = value;
+  this.length++;
+};
+
+Queue.prototype.dequeue = function(){
+  if (this.length > 0){
+    this.length--;
+    var result = this.storage[0];
+    delete this.storage[0];
+    for (var key in this.storage){
+      var temp = this.storage[key];
+      key = parseInt(key) - 1;
+      this.storage[key] = temp;
+    }
+  }
+  return result;
+};
+
+Queue.prototype.size = function(){
+  return this.length;
 };
 
 
