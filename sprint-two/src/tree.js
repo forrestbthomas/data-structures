a781e9ee5a62dec9fc789cb2c4b931378006b0a6
Hack Reactor Students <forrestbthomas@gmail.com>
2014-06-06 21:37:41 -0700

    Passed tree and learned about returning with recursion

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,20 +1,40 @@
 var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
+
+  extend(newTree, treeMethods);
+  //console.log(newTree);
   return newTree;
 };
 
-
+var extend = function(to, from) {
+  for(var key in from) {
+    to[key] = from[key];
+  }
+  return to;
+}
 
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(makeTree(value));
 };
 
 treeMethods.contains = function(target){
+  var result = false;
+  var search = function(node){
+    for (var i = 0; i < node.children.length; i++){
+      if (node.children[i].value === target){
+        result = true;
+      } else {
+        search(node.children[i]);
+      }
+    }
+  };
+  search(this);
+  return result;
 
 };
 
