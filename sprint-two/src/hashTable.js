50a290067428067b301f3eef058323d5a2293895
Hack Reactor Students <forrestbthomas@gmail.com>
2014-06-06 18:29:11 -0700

    passed hash-table with style

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,31 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var arr = this._storage.get(i);
+  if (arr === undefined){
+    arr = [];
+  }
+  var subArray = [k,v];
+  arr.push(subArray);
+  this._storage.set(i, arr);
+
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var arr = this._storage.get(i);
+  if (arr === null) return null;
+  for (var i = 0; i < arr[i].length; i++){
+    if (arr[i][0] === k){
+      return arr[i][1];
+    }
+  }
+  return false;
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(i, null);
 };
 
 
